6f37a878a8cb8f7554972393a882406e4f20037c
Vadim Kiniabaev <thekinv21@gmail.com>
2024-09-15 19:19:14 +0300

    FEAT:[JS-VARIABLES][Vadim]: js-code tutorial variable eklendi

---
+++ 1.variables/variables.js
@@ -0,0 +1,25 @@
+// const Examples
+
+const myName = ' John'
+
+console.log('Const variable name:', myName)
+
+// let Examples
+
+let myCity = 'New York'
+
+console.log('Let variable city in first:', myCity)
+
+myCity = 'Los Angeles'
+
+console.log('Let variable city after change:', myCity)
+
+// var Examples
+
+var myCar = 'Toyota'
+
+console.log('Var variable car:', myCar)
+
+myCar = 'Honda'
+
+console.log('Var variable car after change:', myCar)
